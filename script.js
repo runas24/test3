e8a124225e8f457447140518208a61495ea73762
runas24 <166917824+runas24@users.noreply.github.com>
2024-04-17 19:20:35 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 
+const loadingContainer = document.getElementById('loading-container');
+const loadingBar = document.getElementById('loading-bar');
+const loadingText = document.getElementById('loading-text');
+
+window.addEventListener('load', () => {
+    setTimeout(() => {
+        loadingContainer.classList.add('hidden');
+    }, 1000);
+});
