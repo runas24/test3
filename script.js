c2547f78be68adcc06255526a6fab32355bc6ebb
runas24 <166917824+runas24@users.noreply.github.com>
2024-04-17 19:34:29 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-const coinsContainer = document.querySelector('.coins');
 
-function createCoin() {
-  const coin = document.createElement('div');
-  coin.classList.add('coin');
-  coin.style.left = `${Math.random() * 100}%`;
-  coinsContainer.appendChild(coin);
-  setTimeout(() => {
-    coin.remove();
-  }, 1500);
-}
-
-setInterval(createCoin, 200);
